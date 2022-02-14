8c48e3202b9080e5e7008d1fc4dcf3c9a3a35776
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2022-02-14 18:02:49 -0500

    added line wrap and set font

--- TextEditor.java
+++ TextEditor.java
@@ -1,16 +1,32 @@
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class TextEditor extends JFrame implements ActionListener{
+	
+	
+	JTextArea textArea;
+	
 	TextEditor(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Bro text Editor");
+		this.setTitle("Xavier");
 		this.setSize(500, 500);
 		this.setLayout(new FlowLayout());
 		this.setLocationRelativeTo(null);
+		
+		
+		textArea = new JTextArea();
+		textArea.setPreferredSize(new Dimension(450,450));
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setFont(new Font("Arial",Font.PLAIN,20));
+		
+		this.add(textArea);
 		this.setVisible(true);
 	}
 
